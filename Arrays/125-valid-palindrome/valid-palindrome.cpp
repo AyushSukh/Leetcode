101705cb82d196b3db1e1d75e7b04a196102efe9
Ayush Sukhwal <51879443+AyushSukh@users.noreply.github.com>
2025-05-08 11:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (94.42%) - LeetSync

--- Arrays/125-valid-palindrome/valid-palindrome.cpp
+++ Arrays/125-valid-palindrome/valid-palindrome.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    bool isAlphaNumeric(char c) {
-        return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
-               (c >= '0' && c <= '9');
-    }
-
-    char toLowercase(char c) {
-        if (c >= 'A' && c <= 'Z')
-            return (c - 'A' + 'a');
-        return c;
-    }
-
     bool isPalindrome(string s) {
         int start = 0;
         int end = s.size() - 1;
 
         while (start < end) {
-            while (start < end && !isAlphaNumeric(s[start]))
+            if (!isalnum(s[start])) {
                 start++;
-            while (start < end && !isAlphaNumeric(s[end]))
+                continue;
+            } else if (!isalnum(s[end])) {
                 end--;
-
-            if (toLowercase(s[start++]) != toLowercase(s[end--]))
+                continue;
+            } else if (tolower(s[start]) != tolower(s[end])) {
                 return false;
+            } else {
+                start++;
+                end--;
+            }
         }
         return true;
     }
