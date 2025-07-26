6b7fe6e548a8cfab59b664cafbc43561bc05ef04
Ayush Sukhwal <51879443+AyushSukh@users.noreply.github.com>
2025-07-27 03:10:08 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (95.79%) - LeetSync

--- Arrays/73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ Arrays/73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,44 +1,26 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
-        bool flag1 = false;
-        bool flag2 = false;
-        for (int i = 0; i < n; i++) {
-            if (matrix[i][0] == 0) {
-                flag1 = true;
-            }
-        }
-        for (int j = 0; j < m; j++) {
-            if (matrix[0][j] == 0) {
-                flag2 = true;
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<bool> row(m, false);
+        vector<bool> col(n, false);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
-            }
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-                    matrix[i][j] = 0;
+                    row[i] = true;
+                    col[j] = true;
                 }
             }
         }
 
-        if (flag1 == true) {
-            for (int i = 0; i < n; i++) {
-                matrix[i][0] = 0;
-            }
-        }
-        if (flag2 == true) {
-            for (int j = 0; j < m; j++) {
-                matrix[0][j] = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] == true || col[j] == true) {
+                    matrix[i][j] = 0;
+                }
             }
         }
     }
